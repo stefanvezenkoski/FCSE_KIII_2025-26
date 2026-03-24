b361314214fe066fa23ba473b9f37fd8a76c3f6a
stefanvezenkoski <stefan.vezenkoski@students.finki.ukim.mk>
2026-03-24 17:01:15 +0100

    first commit

---
+++ src/main/java/mk/ukim/finki/fcse_kiii/FcseKiiiApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.fcse_kiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcseKiiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcseKiiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/fcse_kiii/FcseKiiiApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.fcse_kiii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcseKiiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
