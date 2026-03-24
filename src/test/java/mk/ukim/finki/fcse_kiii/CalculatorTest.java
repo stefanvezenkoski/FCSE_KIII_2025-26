b39db6c1b345bea1bc878393ad618ec66d69e679
stefanvezenkoski <stefan.vezenkoski@students.finki.ukim.mk>
2026-03-25 00:50:02 +0100

    change CalculatorTest.java

--- src/test/java/mk/ukim/finki/fcse_kiii/CalculatorTest.java
+++ src/test/java/mk/ukim/finki/fcse_kiii/CalculatorTest.java
@@ -8,6 +8,6 @@ public class CalculatorTest {
     @Test
     void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(51, calc.add(2, 3));
+        assertEquals(5, calc.add(2, 3));
     }
 }
